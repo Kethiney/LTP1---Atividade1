6efa40162a70aa003018648aefc5c28422226531
Kethiney Lauana <101286758+Kethiney@users.noreply.github.com>
2022-03-09 19:05:11 +0000

    Eu coloquei um novo objeto, que chama - objetoNovo; linha 18 calculei a area dele.

--- index.js
+++ index.js
@@ -14,3 +14,6 @@ class Retangulo{
 var quadrado = new Retangulo (10,10); 
 let retangulo = new Retangulo(30,60);
 console.log (quadrado.calcularArea(), retangulo.calcularArea())
+
+let objetoNovo = new Retangulo(20,40)
+console.log(objetoNovo.calcularArea())
